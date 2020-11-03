d22eb606e7cc1271588df2468d32e62ebd11b657
gaurav5991 <gauravharendersharma@gmail.com>
2020-11-03 22:28:09 +0530

    Gaurav[ADDED]: Test Case to check the number of Contact retrieved from Address book Database

--- src/test/java/com/bridgelabz/addressbook/AddressBookMainTest.java
+++ src/test/java/com/bridgelabz/addressbook/AddressBookMainTest.java
@@ -1,4 +1,19 @@
 package com.bridgelabz.addressbook;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+import static com.bridgelabz.addressbook.AddressBookService.IOService.DB_IO;
+
 public class AddressBookMainTest {
+
+    /*Test Case to check the number of retrieved contacts from database*/
+    @Test
+    public void givenAdddressBook_WhenRecordsRetrieved_ShouldReturnCount() throws AddressBookException {
+        AddressBookService addressBookService = new AddressBookService();
+        List<Contact> contactList = addressBookService.readAddressBookData(DB_IO);
+        Assert.assertEquals(3,contactList.size());
+    }
 }
