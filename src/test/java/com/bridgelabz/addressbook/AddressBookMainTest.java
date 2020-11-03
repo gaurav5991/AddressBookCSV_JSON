661cfd56cc68a99a5339c36105771f0146a5d6eb
gaurav5991 <gauravharendersharma@gmail.com>
2020-11-04 01:13:04 +0530

    Gaurav[ADDED]: Test Case to retrieve number of contacts added within given Date Range

--- src/test/java/com/bridgelabz/addressbook/AddressBookMainTest.java
+++ src/test/java/com/bridgelabz/addressbook/AddressBookMainTest.java
@@ -3,6 +3,7 @@ package com.bridgelabz.addressbook;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static com.bridgelabz.addressbook.AddressBookService.IOService.DB_IO;
@@ -24,5 +25,16 @@ public class AddressBookMainTest {
         addressBookService.updateContactInformation("Alex","Carey");
         boolean result = addressBookService.checkEmployeePayrollInSyncWithDB("Alex");
         Assert.assertTrue(result);
+    }
+
+    /* TestCase to check the Contact count who added in given date range*/
+    @Test
+    public void givenDateRangeWhenRetrievedShouldMatchContactCount() throws AddressBookException {
+        AddressBookService addressBookService = new AddressBookService();
+        addressBookService.readAddressBookData(DB_IO);
+        LocalDate startDate = LocalDate.of(2019,01,01);
+        LocalDate endDate = LocalDate.now();
+        List<Contact> contactList = addressBookService.readContactDataForDateRange(DB_IO, startDate, endDate);
+        Assert.assertEquals(2,contactList.size());
     }
 }
