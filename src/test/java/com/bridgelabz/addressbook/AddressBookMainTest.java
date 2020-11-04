09584d2d4d9d6e07f93266fd4936eb203555cfdb
gaurav5991 <gauravharendersharma@gmail.com>
2020-11-05 00:57:43 +0530

    Gaurav[ADDED]: Test Case to add Contact to AddressBookDB

--- src/test/java/com/bridgelabz/addressbook/AddressBookMainTest.java
+++ src/test/java/com/bridgelabz/addressbook/AddressBookMainTest.java
@@ -16,14 +16,15 @@ public class AddressBookMainTest {
     public void givenAdddressBook_WhenRecordsRetrieved_ShouldReturnCount() throws AddressBookException {
         AddressBookService addressBookService = new AddressBookService();
         List<Contact> contactList = addressBookService.readAddressBookData(DB_IO);
-        Assert.assertEquals(3,contactList.size());
+        Assert.assertEquals(6, contactList.size());
     }
+
     /*Test Case to check the contact details updated in database using JDBC*/
     @Test
     public void givenNewInformationOfContact_WhenUpdatedShouldMatch() throws AddressBookException {
         AddressBookService addressBookService = new AddressBookService();
         List<Contact> contactList = addressBookService.readAddressBookData(DB_IO);
-        addressBookService.updateContactInformation("Alex","Carey");
+        addressBookService.updateContactInformation("Alex", "Carey");
         boolean result = addressBookService.checkEmployeePayrollInSyncWithDB("Alex");
         Assert.assertTrue(result);
     }
@@ -33,25 +34,37 @@ public class AddressBookMainTest {
     public void givenDateRangeWhenRetrievedShouldMatchContactCount() throws AddressBookException {
         AddressBookService addressBookService = new AddressBookService();
         addressBookService.readAddressBookData(DB_IO);
-        LocalDate startDate = LocalDate.of(2019,01,01);
+        LocalDate startDate = LocalDate.of(2019, 01, 01);
         LocalDate endDate = LocalDate.now();
         List<Contact> contactList = addressBookService.readContactDataForDateRange(DB_IO, startDate, endDate);
-        Assert.assertEquals(2,contactList.size());
+        Assert.assertEquals(5, contactList.size());
     }
+
     /*Test Case to check the number of retrieved contacts from database by state Name*/
     @Test
     public void givenStateNameWhenRetrievedShouldReturnCorrectMatch() throws AddressBookException {
         AddressBookService addressBookService = new AddressBookService();
         addressBookService.readAddressBookData(DB_IO);
-        Map<String,String> ContactByState = addressBookService.readContactByState(DB_IO);
+        Map<String, String> ContactByState = addressBookService.readContactByState(DB_IO);
         Assert.assertTrue(ContactByState.get("Wales").equals("Michael"));
     }
+
     /*Test Case to check the number of retrieved contacts from database by City Name*/
     @Test
     public void givenCityNameWhenRetrievedShouldReturnCorrectMatch() throws AddressBookException {
         AddressBookService addressBookService = new AddressBookService();
         addressBookService.readAddressBookData(DB_IO);
-        Map<String,String> ContactByCity = addressBookService.readContactByCity(DB_IO);
+        Map<String, String> ContactByCity = addressBookService.readContactByCity(DB_IO);
         Assert.assertTrue(ContactByCity.get("Bradford").equals("Michael"));
+    }
+
+    /*Test Case to check employee when Added to AddressBook DataBase*/
+    @Test
+    public void givenNewContactWhenAddedShouldSyncWithDB() throws AddressBookException {
+        AddressBookService addressBookService = new AddressBookService();
+        addressBookService.readAddressBookData(DB_IO);
+        addressBookService.addContactToAddressBook("Adams", "Baker", "Street 2", "Mexico", "California", "123456", "98675453421", "abc@gmail.com", LocalDate.now(), 110, 210, "friend");
+        boolean result = addressBookService.checkEmployeePayrollInSyncWithDB("Adams");
+        Assert.assertTrue(result);
     }
 }
